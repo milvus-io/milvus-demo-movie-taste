cd3e09c599d0cbb4409a3d22a4cf904ea573bc28
tumao <tumao@protonmail.com>
2020-09-03 10:11:21 +0800

    update dockerfile

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -4,4 +4,5 @@ export const RECOMMEND_ROUTE = '/recommend';
 export const DETAIL_ROUTE = '/detail';
 
 /* Api */
-export const BASE_URL = 'http://192.168.1.85:8000';
+export const BASE_URL =
+  (window._env_ && window._env_.API_URL) || 'http://192.168.1.85:8000';
